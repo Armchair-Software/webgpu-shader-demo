abfaee8a1fc8bc16172e7ee01639d00d78a2443c
SlowRiot <slowriot@voxelstorm.com>
2024-11-07 18:06:51 +0000

    update from latest master

--- include/webgpu.hpp
+++ include/webgpu.hpp
@@ -126,11 +126,10 @@ class Type { \
 public: \
 	typedef Type S; /* S == Self */ \
 	typedef WGPU ## Type W; /* W == WGPU Type */ \
-	Type(const W& w) : m_raw(w) {} \
-	operator W() { return m_raw; } \
-private: \
-	W m_raw; \
-public:
+	constexpr Type() : m_raw(W{}) {} /* Using default value-initialization */ \
+	constexpr Type(const W& w) : m_raw(w) {} \
+	constexpr operator W() const { return m_raw; } \
+	W m_raw; /* Ideally, this would be private, but then types generated with this macro would not be structural. */
 
 #define ENUM_ENTRY(Name, Value) \
 	static constexpr W Name = (W)Value;
