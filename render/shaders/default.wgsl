31636b14b23a4449a973b54aa51beab6bf7daf65
SlowRiot <slowriot@voxelstorm.com>
2024-11-10 18:24:39 +0000

    add colour transport from vert to frag

--- render/shaders/default.wgsl
+++ render/shaders/default.wgsl
@@ -2,12 +2,21 @@ struct vertex_input {
   @location(0) position: vec2f,
 };
 
+struct fragment_input {
+    @builtin(position) position: vec4f,
+    @location(0) colour: vec4f,
+};
+alias vertex_output = fragment_input;
+
 @vertex
-fn vs_main(in: vertex_input) -> @builtin(position) vec4f {
-  return vec4f(in.position, 0.0, 1.0);
+fn vs_main(in: vertex_input) -> vertex_output {
+  var out: vertex_output;
+  out.position = vec4f(in.position, 0.0, 1.0);
+  out.colour = vec4f(1.0, 1.0, 0.0, 1.0);
+  return out;
 }
 
 @fragment
-fn fs_main() -> @location(0) vec4f {
-  return vec4f(1.0, 1.0, 0.0, 1.0);
+fn fs_main(in: fragment_input) -> @location(0) vec4f {
+  return in.colour;
 }
--- render/shaders/default.wgsl.h
+++ render/shaders/default.wgsl.h
@@ -1,16 +1,25 @@
 namespace render::shaders {
-inline constexpr char const *default_wgsl{R"fafac6093d01969a(struct vertex_input {
+inline constexpr char const *default_wgsl{R"af74c7699af757e5(struct vertex_input {
   @location(0) position: vec2f,
-  //@location(1) color: vec3f,
+  //@location(1) colour: vec4f,
 };
 
+struct fragment_input {
+    @builtin(position) position: vec4f,
+    @location(0) colour: vec4f,
+};
+alias vertex_output = fragment_input;
+
 @vertex
-fn vs_main(in: vertex_input) -> @builtin(position) vec4f {
-  return vec4f(in.position, 0.0, 1.0);
+fn vs_main(in: vertex_input) -> vertex_output {
+  var out: vertex_output;
+  out.position = vec4f(in.position, 0.0, 1.0);
+  out.colour = vec4f(1.0, 1.0, 0.0, 1.0);
+  return out;
 }
 
 @fragment
-fn fs_main() -> @location(0) vec4f {
-  return vec4f(1.0, 1.0, 0.0, 1.0);
+fn fs_main(in: fragment_input) -> @location(0) vec4f {
+  return in.colour;
 }
-)fafac6093d01969a"};}
+)af74c7699af757e5"};}
