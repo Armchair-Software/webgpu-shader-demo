0c3f68a18586b2521522e135eb81697dcd003725
SlowRiot <slowriot@voxelstorm.com>
2024-11-22 16:50:01 +0000

    remove unused enum

--- main.cpp
+++ main.cpp
@@ -31,25 +31,6 @@ struct gamepad {
 };
 
 
-enum class emscripten_gamepad_button : unsigned int {
-  a,
-  b,
-  x,
-  y,
-  left_bumper,
-  right_bumper,
-  back,
-  start,
-  guide,
-  left_thumb,
-  right_thumb,
-  dpad_up,
-  dpad_right,
-  dpad_down,
-  dpad_left
-};
-
-
 class game_manager {
   logstorm::manager logger{logstorm::manager::build_with_sink<logstorm::sink::console>()}; // logging system
   render::webgpu_renderer renderer{logger};                                     // WebGPU rendering system
