60ff85b98533a886ceb8257eb5d97ed18e2aa3f5
SlowRiot <slowriot@voxelstorm.com>
2024-11-27 15:29:14 +0000

    enable webgpu and vulkan

--- electron/webgpu-demo3/src/main.js
+++ electron/webgpu-demo3/src/main.js
@@ -2,6 +2,10 @@ import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 
+app.commandLine.appendSwitch('enable-unsafe-webgpu');
+app.commandLine.appendSwitch('enable-features', 'Vulkan');
+//app.commandLine.appendSwitch('disable-color-correct-rendering');
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
