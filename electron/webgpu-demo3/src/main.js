d258cdc5e74e1f12cbbc94b456c932e384f4f66e
SlowRiot <slowriot@voxelstorm.com>
2024-11-27 15:36:01 +0000

    force srgb rendering

--- electron/webgpu-demo3/src/main.js
+++ electron/webgpu-demo3/src/main.js
@@ -5,6 +5,7 @@ import started from 'electron-squirrel-startup';
 app.commandLine.appendSwitch('enable-unsafe-webgpu');
 app.commandLine.appendSwitch('enable-features', 'Vulkan');
 //app.commandLine.appendSwitch('disable-color-correct-rendering');
+app.commandLine.appendSwitch('force-color-profile', 'srgb');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
